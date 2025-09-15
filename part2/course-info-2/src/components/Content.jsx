5bcf0eab7f8b8b6ccad59914c7a6a0c380336890
annurbari <annurBari@student.unsw.edu.au>
2025-09-16 00:05:35 +1000

    App.jsx

--- part2/course-info-2/src/components/Content.jsx
+++ part2/course-info-2/src/components/Content.jsx
@@ -1,9 +1,11 @@
 import Part from "./Part"
 
 const Content = ({ parts }) => {
+    const total = parts.reduce((accumulator, currNum) => accumulator + currNum.exercises, 0)
     return (
     <>
         {parts.map((part, index) => <Part key={index} partObj={part}/>)}
+        <strong>total of {total} exercises</strong>
     </>
   )
 }
