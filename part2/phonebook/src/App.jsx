9a399151e188410376860b19a3f236150744a96d
annurbari <annurBari@student.unsw.edu.au>
2025-10-01 00:51:25 +1000

    phonebook exercise complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/notes'
+import Notification from './components/Notification'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [errorType, setErrorType] = useState('error')
+
+  useEffect(() => {
+    personService
+      .getPersons()
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const matchedPersons = persons.filter(function(person, index) {
     return person.name.toLowerCase().includes(filter.toLowerCase())
@@ -22,24 +28,61 @@ const App = () => {
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleFilter = (event) => setFilter(event.target.value)
+  const displayNotifications = (message, type) => {
+    setErrorMessage(message)
+    setErrorType(type)
+    setTimeout(() => {
+    setErrorMessage('')
+    }, 5000)
+  }
+  const completePersonAddition = () => {
+    let nameObj = { name: newName, number: newNumber }
+    personService
+      .addPerson(nameObj)
+      .then(res1 => {
+        return personService.getPersons()
+      })
+      .then(res2 => {
+        setPersons(res2.data)
+        displayNotifications(`Added ${newName}`, 'success')
+      })
+      .catch(error => {
+        setErrorMessage(`Information about ${newName} has already been removed from the server`)
+      })
+  }
+
+  const completePersonUpdate = (id) => {
+    let nameObj = { name: newName, number: newNumber }
+    personService
+      .updatePerson(id, nameObj)
+      .then(res1 => {
+        return personService.getPersons()
+      })
+      .then(res2 => {
+        setPersons(res2.data)
+        displayNotifications(`Updated ${newName}'s number to ${newNumber}`, 'success')
+      })
+      .catch(error => {
+        displayNotifications(`Information about ${newName} has already been removed from the server`, 'error')
+      })
+  }
 
   const handleAddClick = (event) => {
     event.preventDefault()
     const nameExists = persons.find((person) => newName === person.name)
     if (nameExists) {
-      alert(`${newName} is already added to the phonebook`)
+      if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
+        completePersonUpdate(nameExists.id)
+      }
     } else {
-      const nameObj = { name: newName, number: newNumber }
-      const personsArr = [...persons, nameObj]
-      setPersons(personsArr)
-      setNewName('')
-      setNewNumber('')
+      completePersonAddition()
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} type={errorType}/>
       <Filter value={filter} eventHandler={handleFilter}/>
       <h2>add a new</h2>
       <PersonForm
@@ -50,7 +93,7 @@ const App = () => {
         addEventHandler={handleAddClick}
       />
       <h2>Numbers</h2>
-      <Persons matchedPersons={matchedPersons}/>
+      <Persons matchedPersons={matchedPersons} allPersons={persons} setAllPersons={setPersons}/>
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,8 +1,25 @@
-const Persons = ({ matchedPersons }) => {
+import axios from "axios"
+import personService from '../services/notes'
+
+const Persons = ({ matchedPersons, allPersons, setAllPersons }) => {
+    const handleDeletePerson = (id, name) => {
+        if (window.confirm(`Delete ${name}`)) {
+            personService
+                .deletePerson(id)
+                .then(response => {
+                    personService
+                        .getPersons()
+                        .then(response => {
+                            setAllPersons(response.data)
+                        })
+                })
+        }
+    }
+
     return (
         <>
             {matchedPersons.map(function(person, index) {
-                return <span key={index}>{person.name} {person.number}<br /></span>
+                return <span key={index}>{person.name} {person.number}<button onClick={() => handleDeletePerson(person.id, person.name)}>delete</button><br /></span>
             })}
         </>
     )
