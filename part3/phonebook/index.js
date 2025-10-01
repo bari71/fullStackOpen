48ed937f5bd82eae4e441de81876030631ecb9bc
annurbari <annurBari@student.unsw.edu.au>
2025-10-02 01:22:37 +1000

    done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 
 let persons = [
     {
@@ -106,7 +108,7 @@ const findMaxNum = () => {
         : 0
     return (String(maxNum + 1))
 }
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
