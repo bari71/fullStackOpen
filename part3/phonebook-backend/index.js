9f55f92b62169a8f98f75d4d3efa8b13122574c7
annurbari <annurBari@student.unsw.edu.au>
2025-10-04 08:07:05 +1000

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,39 +1,22 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
+const password = process.argv[2]
+
 const app = express()
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    {
-      "id": "1",
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": "2",
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": "3",
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": "4",
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-]
-
 app.use(express.json())
 app.use(morgan('tiny'))
 
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(results => {
+        console.log(results)
+        res.json(results)
+    })
 })
 
 app.get('/info', (req, res) => {
@@ -42,20 +25,9 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons/:id', function(req, res) {
-    const id = req.params.id
-    const person = persons.find(function(p) {
-        if (p.id === id) {
-            return true
-        } else {
-            return false
-        }
-    })
-
-    if (person) {
+    Person.findById(req.params.id).then(person => {
         res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', function(req, res) {
@@ -77,20 +49,18 @@ app.post('/api/persons', function(req, res) {
             error: 'name/number missing'
         })
     }
-    if (checkNameExists(body.name)) {
-        return res.status(400).json({
-            error: 'Name already exists in phonebook'
-        })
-    }
-    const person = {
-        id: findMaxNum(),
+    // if (checkNameExists(body.name)) {
+    //    return res.status(400).json({
+    //        error: 'Name already exists in phonebook'
+    //    })
+    //}
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
-
-    persons = persons.concat(person)
-    res.json(person)
-    console.log(persons)
+    })
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 const checkNameExists = (name) => {
@@ -109,7 +79,7 @@ const findMaxNum = () => {
         : 0
     return (String(maxNum + 1))
 }
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
